14ad43e73d3ba03a8f49915fa230990b8ebaabc5
Jeff Lucas <jslucas1@msn.com>
2019-02-07 16:29:35 -0600

    Initial Commit2

---
+++ src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    //The driver code for the menu goes in main
+        //Create keyboard object
+        Scanner keyboard = new Scanner(System.in);
+
+
+    }
+
+    /**
+     * This method is used to display the menu options
+     * The user can then make a pick.  That choice is returned to the main method
+     * @param keyboard
+     * @return the menu choice
+     */
+    public static String getMenuChoice(Scanner keyboard){
+
+
+    }
+
+    /**
+     * This method is used to disply the triangle when called from the main menu
+     */
+    public static void displayTriangle(){
+
+    }
+
+    /**
+     * This method is used to display the table of numbers when called from the main menu
+     * Use the tab escape sequence to get output to line up i.e. ("\t")
+     */
+    public static void displayChart(){
+
+    }
+}
