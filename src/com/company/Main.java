8e1a69b7229c55d271b0c033cd5386f83a2e0103
kevin <kjmiller6@crimson.ua.edu>
2019-02-08 17:30:34 -0600

    added code for menus and complete projects

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,26 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	    //The driver code for the menu goes in main
+        //The driver code for the menu goes in main
         //Create keyboard object
         Scanner keyboard = new Scanner(System.in);
 
+        String input = getMenuChoice(keyboard);
+        while (!input.equals("3")) {
+            switch (input) {
+                case "1":
+                    displayTriangle();
+                    break;
+                case "2":
+                    displayChart();
+                    break;
+                default:
+                    System.out.println("Please enter an actual option");
 
+
+            }
+            input = getMenuChoice(keyboard);
+        }
     }
 
     /**
@@ -20,14 +35,27 @@ public class Main {
      */
     public static String getMenuChoice(Scanner keyboard){
 
-
+        System.out.println("Please enter your choice");
+        return keyboard.next();
     }
 
     /**
      * This method is used to disply the triangle when called from the main menu
      */
     public static void displayTriangle(){
+        int i, j;
+        int rows = 10;
 
+        for(i=1; i<=rows; i++)
+        {
+
+            for(j=i; j<=rows; j++)
+            {
+                System.out.print("*");
+            }
+
+            System.out.println("");
+        }
     }
 
     /**
@@ -36,5 +64,14 @@ public class Main {
      */
     public static void displayChart(){
 
+        for (int i=1; i<5; i++) {
+            System.out.print(i + " ");
+
+            for (int j=0; j<10; j++)
+            {
+                System.out.print(i * j + " ");
+            }
+            System.out.println("");
+        }
     }
 }
